81269801ad2761e5530942a8adb9abcda0330bb3
Danilo B de Souza <75020761+analystuni9@users.noreply.github.com>
2024-04-03 06:51:25 -0300

    Adicionando arquivos de JS no repositório.
    Exploração e prática de JavaScript, abrangendo desde fundamentos básicos até conceitos avançados, com foco em desenvolvimento web e aplicações interativas.

---
+++ curso_js_sfe/secao_2_revisao_logica_de_programacao/14_tipos_de_dados_primitivos.js
@@ -0,0 +1,15 @@
+// Strings, números (inteiros, ponto flutuante) e booleanos são tipos de dados em JavaScript.
+
+// Nas minhas variáveis, posso usar letras, números e símbolos.
+
+let variavel = "minha \"string\" com aspas duplas"; // Exemplo de uma string com aspas duplas, usando escape para incluir aspas duplas dentro da string.
+let variavel2 = 'minha string com aspas simples'; // Exemplo de uma string com aspas simples.
+var variavel3 = `minha string com template literal`; // Exemplo de uma string com template literal, permitindo interpolação de variáveis.
+
+console.log(variavel); // Imprime a variável 'variavel' no console.
+let idade = 40; // Exemplo de uma variável do tipo número (inteiro).
+let mensagem = "eu tenho " + idade + " anos"; // Exemplo de concatenação de strings e variáveis para formar uma mensagem.
+let mensagem2 = `eu tenho ${idade} anos`; // Exemplo de uso de template literals para incluir variáveis em uma string.
+// console.log(mensagem); // Imprime a mensagem no console.
+console.log(mensagem2); // Imprime a mensagem2 no console.
+console.log("hello " + "world"); // Concatenação de duas strings.
